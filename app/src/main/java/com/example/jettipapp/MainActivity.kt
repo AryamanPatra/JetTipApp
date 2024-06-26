6b36c9967712318937ee450e06b2be3df93b7e77
Aryaman Patra <aryamanprivate26@gmail.com>
2024-06-26 14:12:57 +0530

    Udemy tutorial Container

--- app/src/main/java/com/example/jettipapp/MainActivity.kt
+++ app/src/main/java/com/example/jettipapp/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.jettipapp
 
-import android.graphics.fonts.Font
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,9 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -24,81 +19,64 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.jettipapp.ui.theme.JetTipAppTheme
-import com.example.jettipapp.ui.theme.poppinsFontFamily
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetTipAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CreateJetTipApp(innerPadding)
-                }
+            CreateJetTipApp{ innerPadding ->
+                TotalPerPerson(innerPadding)
+                CalculateTip()
             }
         }
     }
 }
 
 @Composable
-fun CreateJetTipApp(innerPadding: PaddingValues = PaddingValues()) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        TotalPerPerson()
-        CalculateTip()
+fun CreateJetTipApp(content: @Composable (innerPadding: PaddingValues) -> Unit) {
+    JetTipAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            content(innerPadding)
+        }
     }
 }
 
 @Preview
 @Composable
-fun TotalPerPerson(){
-    Surface {
-        Card(
+fun TotalPerPerson(innerPadding: PaddingValues = PaddingValues()) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .padding(innerPadding)
+            .clip(shape = RoundedCornerShape(24.dp)),
+        color = Color(0xFFE9D7F7)
+    ) {
+        Column(
             modifier = Modifier
-                .padding(32.dp)
-                .fillMaxWidth()
-                .height(200.dp),
-            shape = RoundedCornerShape(20.dp),
-            colors = CardDefaults.cardColors(Color(0xffe5d2f6)),
+                .fillMaxSize()
+                .padding(12.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = "Total Per Person",
-                        color = Color.Black,
-                        style = MaterialTheme.typography.titleLarge,
-                        fontFamily = poppinsFontFamily,
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 23.sp
-                    )
-                    Text(
-                        text = "$100",
-                        color = Color.Black,
-                        style = MaterialTheme.typography.titleLarge,
-                        fontFamily = poppinsFontFamily,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 35.sp
-                    )
-                }
-            }
+            Text(
+                text = "Total Per Person",
+                color = Color.Black,
+                style = MaterialTheme.typography.headlineMedium,
+            )
+            Text(
+                text = "$100",
+                color = Color.Black,
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.Bold,
+            )
         }
     }
 }
